407b5d86533e87a0b35ef96505733fe5e59b0e4c
Jiahui(Jim) Yang(Young) <jiahuiya@andrew.cmu.edu>
2024-03-21 20:17:55 -0400

    moving all ig files from neural_mp to IsaacGymEnv

--- isaacgymenvs/tasks/__init__.py
+++ isaacgymenvs/tasks/__init__.py
@@ -39,6 +39,8 @@ from .factory.factory_task_nut_bolt_screw import FactoryTaskNutBoltScrew
 from .franka_cabinet import FrankaCabinet
 from .franka_cube_stack import FrankaCubeStack
 from .franka_reach import FrankaReach
+from .franka_mp import FrankaMP
+from .franka_mp_random import FrankaMPRandom
 from .humanoid import Humanoid
 from .humanoid_amp import HumanoidAMP
 from .ingenuity import Ingenuity
@@ -107,6 +109,7 @@ isaacgym_task_map = {
     "FrankaCabinet": FrankaCabinet,
     "FrankaCubeStack": FrankaCubeStack,
     "FrankaReach": FrankaReach,
+    "FrankaMPRandom": FrankaMPRandom,
     "Humanoid": Humanoid,
     "HumanoidAMP": HumanoidAMP,
     "Ingenuity": Ingenuity,
