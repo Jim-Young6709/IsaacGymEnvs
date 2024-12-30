670a6a001faecad04c87ca3ac1ab73ef64fc27fd
Jiahui(Jim) Yang(Young) <jiahuiya@andrew.cmu.edu>
2024-12-30 16:18:18 -0500

    update simple env

--- isaacgymenvs/tasks/franka_mp_simple.py
+++ isaacgymenvs/tasks/franka_mp_simple.py
@@ -119,54 +119,6 @@ class FrankaMPSimple(FrankaMP):
         actor_num = 1 + 1 + 1
         self.init_data(actor_num=actor_num)
 
-    def plan_open_loop(self, use_controller=True):
-        """
-        debug base policy
-        """
-        obs_base = OrderedDict()
-
-        for i in range(150):
-            self.update_robot_pcds()
-            obs_base["current_angles"] = self.states['q'][:, :7].clone()
-            obs_base["goal_angles"] = self.goal_config.clone()
-            obs_base["compute_pcd_params"] = self.combined_pcds.clone()
-            base_action = self.base_model.policy.get_action(obs_dict=obs_base)
-            abs_action = base_action + self.get_joint_angles()
-            if use_controller:
-                self.step(base_action)
-                self._refresh()
-            else:
-                self.set_robot_joint_state(abs_action)
-                self.gym.simulate(self.sim)
-                self.render()
-                self._refresh()
-
-    def visualize_pcd_meshcat(self):
-        "for debug purposes"
-        import meshcat
-        import urchin
-        from robofin.robots import FrankaRobot
-        self.viz = meshcat.Visualizer()
-        self.urdf = urchin.URDF.load(FrankaRobot.urdf)
-        for idx, (k, v) in enumerate(self.urdf.visual_trimesh_fk(np.zeros(8)).items()):
-            self.viz[f"robot/{idx}"].set_object(
-                meshcat.geometry.TriangularMeshGeometry(k.vertices, k.faces),
-                meshcat.geometry.MeshLambertMaterial(wireframe=False),
-            )
-            self.viz[f"robot/{idx}"].set_transform(v)
-        pcd_rgb = np.zeros((3, 8192))
-        pcd_rgb[0, :2048] = 1
-        pcd_rgb[1, 2048:6144] = 1
-        pcd_rgb[2, 6144:] = 1
-        
-        self.viz['pcd'].set_object(
-            meshcat.geometry.PointCloud(
-                position=self.combined_pcds[0, :, :3].cpu().numpy().T,
-                color=pcd_rgb,
-                size=0.005,
-            )
-        )
-
     def reset_idx(self, env_ids: np.ndarray = None):
         if env_ids is None:
             env_ids = np.arange(self.num_envs)
@@ -193,6 +145,8 @@ class FrankaMPSimple(FrankaMP):
         
         self.progress_buf[env_ids] = 0
         self.reset_buf[env_ids] = 0
+        self.base_model.start_episode()
+        self.compute_observations()
 
     def compute_reward(self, actions):
         self.check_robot_collision()
@@ -213,25 +167,6 @@ class FrankaMPSimple(FrankaMP):
         if is_last_step:
             self.extras['successes'] = torch.mean(torch.where(d < 0.1, 1.0, 0.0)).item()
 
-    def pre_physics_step(self, actions):
-        delta_actions = actions.clone().to(self.device)
-        gripper_state = torch.Tensor([[0.035, 0.035]] * self.num_envs).to(self.device)
-
-        delta_actions = torch.clamp(delta_actions, -self.cmd_limit, self.cmd_limit) / self.action_scale
-        # delta_actions = delta_actions * self.cmd_limit / self.action_scale
-        abs_actions = self.get_joint_angles() + delta_actions
-        if abs_actions.shape[-1] == 7:
-            abs_actions = torch.cat((abs_actions, gripper_state), dim=1)
-
-        self.gym.set_dof_position_target_tensor(self.sim, gymtorch.unwrap_tensor(abs_actions))
-
-    def post_physics_step(self):
-        super().post_physics_step()
-        # reset the robot to start if it collides with the obstacles
-        if sum(self.scene_collision) > 0:
-            collision_env_idx = self.collision.nonzero(as_tuple=False).flatten()
-            self.set_robot_joint_state(self.start_config[collision_env_idx], env_ids=collision_env_idx)
-
     def compute_scene_pcds(self):
         # hardcoded for simple env
         cuboids = [
@@ -240,27 +175,9 @@ class FrankaMPSimple(FrankaMP):
         ]
         return construct_mixed_point_cloud(cuboids, self.pcd_spec_dict['num_obstacle_points'])[:, :3]
 
-    def update_robot_pcds(self):
-        num_robot_points = self.pcd_spec_dict['num_robot_points']
-        num_target_points = self.pcd_spec_dict['num_target_points']
-        robot_pcd = self.gpu_fk_sampler.sample(self.get_joint_angles(), num_robot_points)
-        target_pcd = self.gpu_fk_sampler.sample(self.goal_config, num_target_points)
-        self.combined_pcds[:, :num_robot_points, :3] = robot_pcd
-        self.combined_pcds[:, -num_target_points:, :3] = target_pcd
 
 @hydra.main(config_name="config", config_path="../cfg/")
 def launch_test(cfg: DictConfig):
-    import isaacgymenvs
-    from isaacgymenvs.learning import amp_continuous, amp_models, amp_network_builder, amp_players
-    from isaacgymenvs.utils.rlgames_utils import (
-        RLGPUAlgoObserver,
-        RLGPUEnv,
-        get_rlgames_env_creator,
-    )
-    from rl_games.algos_torch import model_builder
-    from rl_games.common import env_configurations, vecenv
-    from rl_games.torch_runner import Runner
-
     np.random.seed(0)
     torch.manual_seed(0)
     cfg_dict = omegaconf_to_dict(cfg)
@@ -281,10 +198,6 @@ def launch_test(cfg: DictConfig):
         t1 = time.time()
         env.reset_idx()
         t2 = time.time()
-        # print(f"Reset time: {t2 - t1}")
-        # print("\nvalidation checking:")
-        # env.set_robot_joint_state(env.start_config)
-        # env.print_resampling_info(env.start_config)
 
         env.render()
 
