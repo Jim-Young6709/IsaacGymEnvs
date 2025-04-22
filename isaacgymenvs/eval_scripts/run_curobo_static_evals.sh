1229795b5965a017d8a417acf91b96d03dbab31f
Jason Liu <jason_liu1116@hotmail.com>
2025-04-22 02:28:20 -0400

    update

--- isaacgymenvs/eval_scripts/run_curobo_static_evals.sh
+++ isaacgymenvs/eval_scripts/run_curobo_static_evals.sh
@@ -2,23 +2,23 @@
 
 # List of task names
 task_names=(
-  # static_box_ff
-  # static_box_ft
-  # static_box_tf
-  # static_box_tt
-  # static_cage_ff
-  # static_cage_ft
-  # static_cage_tf
-  # static_cage_tt
-  # static_cubby_ff
-  # static_cubby_ft
-  # static_cubby_tf
-  # static_cubby_tt
-  # static_dishwasher_ff
-  # static_dishwasher_ft
-  # static_dishwasher_tf
-  # static_dishwasher_tt
-  # static_hybrid_ff
+  static_box_ff
+  static_box_ft
+  static_box_tf
+  static_box_tt
+  static_cage_ff
+  static_cage_ft
+  static_cage_tf
+  static_cage_tt
+  static_cubby_ff
+  static_cubby_ft
+  static_cubby_tf
+  static_cubby_tt
+  static_dishwasher_ff
+  static_dishwasher_ft
+  static_dishwasher_tf
+  static_dishwasher_tt
+  static_hybrid_ff
   static_hybrid_ft
   static_hybrid_tf
   static_hybrid_tt
@@ -74,10 +74,29 @@ task_names=(
 # python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_2" task.use_speed_norm=False
 # python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_3" task.use_speed_norm=False
 
-python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_3" task.use_speed_norm=True
-python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_1" task.use_speed_norm=True
-python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_2" task.use_speed_norm=True
+# python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_3" task.use_speed_norm=True
+# python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_1" task.use_speed_norm=True
+# python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_2" task.use_speed_norm=True
 
 
 
 
+# Curobo Closed Loop Dynamic Goal Blocking without RAP
+python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_1" task.use_speed_norm=True task.use_artificial_potential=False
+python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_2" task.use_speed_norm=True task.use_artificial_potential=False
+python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_3" task.use_speed_norm=True task.use_artificial_potential=False
+python3 run_drp_evals.py headless=True task.planner="Curobo" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_4" task.use_speed_norm=True task.use_artificial_potential=False
+
+
+# Curobo Closed Loop Quasi Dynamic Voxel
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_1" task.use_speed_norm=True task.use_artificial_potential=False
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_2" task.use_speed_norm=True task.use_artificial_potential=False
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_3" task.use_speed_norm=True task.use_artificial_potential=False
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="quasi_dynamic" task.task_name="level_4" task.use_speed_norm=True task.use_artificial_potential=False
+
+
+# Curobo Closed Loop Dynamic Goal Blocking with RAP Voxel
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_1" task.use_speed_norm=True task.use_artificial_potential=True
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_2" task.use_speed_norm=True task.use_artificial_potential=True
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_3" task.use_speed_norm=True task.use_artificial_potential=True
+python3 run_drp_evals.py headless=True task.planner="Curobo_PCD" task.close_loop=True task.task_type="dynamic_goal_blocker" task.task_name="level_4" task.use_speed_norm=True task.use_artificial_potential=True
