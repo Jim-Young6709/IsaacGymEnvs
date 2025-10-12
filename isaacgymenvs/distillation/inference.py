ff8796d55a8368b6d713ae3eaff767f78e65c02a
Jason Liu <jason_liu1116@hotmail.com>
2025-10-11 21:13:06 -0400

    update

--- isaacgymenvs/distillation/inference.py
+++ isaacgymenvs/distillation/inference.py
@@ -3,8 +3,65 @@ from hydra.utils import instantiate
 from omegaconf import OmegaConf
 from pathlib import Path
 from collections import OrderedDict
-from isaacgymenvs.utils.pcd_utils import crop_local_pcd
 from isaacgymenvs.utils.training_utils import *
+
+
+def shuffle_pcd(pcd: torch.Tensor) -> torch.Tensor:
+    """
+    Randomize ordering of points in a point cloud.
+    
+    Args:
+        pcd: (B, N, 3) tensor
+    Returns:
+        shuffled: (B, N, 3) tensor with points randomly permuted
+    """
+    B, N, _ = pcd.shape
+    device = pcd.device
+
+    # Random permutations (different per batch)
+    idx = torch.argsort(torch.rand(B, N, device=device), dim=-1)  # (B, N)
+
+    # Build batch index for gather
+    batch_idx = torch.arange(B, device=device)[:, None].expand(B, N)
+
+    # Gather shuffled points
+    return pcd[batch_idx, idx]  # (B, N, 3)
+
+
+def crop_local_pcd(pcd: torch.Tensor, local_range: torch.float, num_local_points: torch.int):
+    """
+    Crop the point cloud to a local region around the origin with 0 padding.
+    Args:
+        pcd: (B, N, 3) tensor
+        range: float, the radius of the local region
+    """
+    B, N, _ = pcd.shape
+    device = pcd.device
+
+    # get local pcd
+    masked_pcds = shuffle_pcd(pcd)
+    dist = torch.norm(masked_pcds, dim=-1)
+    mask = dist < local_range # nan < X always returns false, so if there are nan values in pcd input, it get automatically filtered out
+    masked_pcds[~mask] = float("nan")
+
+    # sort to get all the valid points
+    is_valid = mask.int()
+    sort_idx = torch.argsort(is_valid, dim=-1, descending=True)
+    batch_idx = torch.arange(B, device=device)[:, None].expand(B, N)
+    sorted_pcds = masked_pcds[batch_idx, sort_idx]  # (B, N, 3)
+    pcd_local_nan_padding = sorted_pcds[:, :num_local_points]
+
+    avg_num_valid_points = is_valid.sum() / B
+    min_num_valid_points = is_valid.sum(dim=-1).min()
+    logs = {
+        "local_crop/avg_num_valid_points": avg_num_valid_points.item(),
+        "local_crop/min_num_valid_points": min_num_valid_points.item(),
+    }
+
+    # replace nan values as 0s
+    local_pcd_zero_padding = torch.nan_to_num(pcd_local_nan_padding, nan=0.0)
+
+    return local_pcd_zero_padding, logs
 
 
 @torch.jit.script
