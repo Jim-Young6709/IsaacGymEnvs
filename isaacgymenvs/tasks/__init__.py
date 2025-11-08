5f27ebf66a6ec02927eba7b0057444f69f7a3f84
Jim-Young6709 <jiahuiyang6709@outlook.com>
2025-11-07 23:52:33 -0500

    add configs

--- isaacgymenvs/tasks/__init__.py
+++ isaacgymenvs/tasks/__init__.py
@@ -33,8 +33,12 @@ from .franka_leap_pick_side import FrankaLEAPPickSide
 from .franka_leap_pick_full import FrankaLEAPPickFull
 from .franka_leap_pick_top_full import FrankaLEAPPickTopFull
 
+from .franka_leap_mobile import FrankaLEAPMobile
+from .franka_leap_mobile_pick_top_full import FrankaLEAPMobilePickTopFull
+
 from .franka_cmd import FrankaCMD
 from .franka_cmd_pick_table import FrankaCMDPickTable
+
 
 
 # Mappings from strings to environments
@@ -45,6 +49,8 @@ isaacgym_task_map = {
     "FrankaLEAPPickSide": FrankaLEAPPickSide,
     "FrankaLEAPPickFull": FrankaLEAPPickFull,
     "FrankaLEAPPickTopFull": FrankaLEAPPickTopFull,
+    "FrankaLEAPMobile": FrankaLEAPMobile,
+    "FrankaLEAPMobilePickTopFull": FrankaLEAPMobilePickTopFull,
     "FrankaCMD": FrankaCMD,
     "FrankaCMDPickTable": FrankaCMDPickTable,
 }
