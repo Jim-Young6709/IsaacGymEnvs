51bd893774652f9f3f48d3d41f6eece471e3cd4c
Jim-Young6709 <jiahuiyang6709@outlook.com>
2025-11-23 11:39:06 -0500

    cleanup

--- isaacgymenvs/distillation/run_dagger.py
+++ isaacgymenvs/distillation/run_dagger.py
@@ -3,13 +3,19 @@ import torch
 from omegaconf import DictConfig
 import hydra
 from isaacgymenvs.distillation.dagger_trainer import Dagger
+from isaacgymenvs.distillation.dagger_mobile_trainer import DaggerMobile
 
 
 @hydra.main(config_name="dagger_config", config_path="../cfg")
 def main(cfg: DictConfig):
-    dagger_trainer = Dagger(
-        cfg=cfg,
-    )
+    if 'Mobile' in cfg['task']['name']:
+        dagger_trainer = DaggerMobile(
+            cfg=cfg,
+        )
+    else:
+        dagger_trainer = Dagger(
+            cfg=cfg,
+        )
 
     dagger_trainer.train()
 
