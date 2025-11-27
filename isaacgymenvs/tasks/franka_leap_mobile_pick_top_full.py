b5506b0624f905aff4caf82c40e259151b0eccfc
Jim-Young6709 <jiahuiyang6709@outlook.com>
2025-11-27 00:16:31 -0500

    tune view angle

--- isaacgymenvs/tasks/franka_leap_mobile_pick_top_full.py
+++ isaacgymenvs/tasks/franka_leap_mobile_pick_top_full.py
@@ -698,8 +698,8 @@ class FrankaLEAPMobilePickTopFull(FrankaLEAPMobile):
 
     def set_viewer(self):
         super().set_viewer(
-            pos=[1.0, 0.0, 1.0],
-            target=[0.5, 0.0, 0.1],
+            pos=[1.5, 0.0, 1.0],
+            target=[0.3, 0.0, 0.7],
         )
 
 
