2863ae0a8d3af66c8f65c2a3442739bbb8f60185
Jim-Young6709 <jiahuiyang6709@outlook.com>
2025-12-11 06:16:05 -0500

    wbc inference code version 1 completed

--- isaacgymenvs/inference/inference_wbc_policy.py
+++ isaacgymenvs/inference/inference_wbc_policy.py
@@ -12,13 +12,18 @@ from isaacgymenvs.inference.inference_utils import *
 TRANSFORMER_CONFIGS = {
     "device": "cuda:0",
     "clip_actions": 1.0,
-    "local_pcd_range": 0.5,
-    "num_local_points": 512,
+    "local_pcd_range": 1.5,
+    "num_local_points": 1024,
     "seed": 42,
     "action_scale": {
-        "eef_pos": 0.01,
-        "eef_rot": 0.01,
-        "hand": 0.05,
+        "franka": 0.01,
+        "arx": 0.01,
+        "hand": 0.5,
+    },
+    "action_space": {
+        "delta_franka_action": True,
+        "delta_leap_action": True,
+        "delta_arx_action": True,
     },
     "model_config_path": "isaacgymenvs/cfg/model/deploy_transformer_mobile_fullscene_t.yaml",
     "ckpt_path": "dagger_ckpts/grogu_ckpts/Dec11_wbc_table_delta_nodr.pt",
@@ -33,6 +38,10 @@ class WBCPolicyTransformer:
         self.num_local_points = configs["num_local_points"]
         self.clip_actions = configs["clip_actions"]
         self.action_scale = configs["action_scale"]
+
+        self.delta_franka_action = configs["action_space"]["delta_franka_action"]
+        self.delta_leap_action = configs["action_space"]["delta_leap_action"]
+        self.delta_arx_action = configs["action_space"]["delta_arx_action"]
 
         # load model from config
         model_config_file = Path(configs["model_config_path"])
@@ -49,21 +58,30 @@ class WBCPolicyTransformer:
             colorprint(f"Loading ckpt from {load_checkpoint_path}: success_rate_ep={success_rate_ep}", color="magenta")
 
         self.robot_dof_lower_limits = torch.tensor([
-            -2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973,
-            -0.3140, -1.0470, -0.5060, -0.3660,
-            -0.3490, -0.4700, -1.2000, -1.3400,
-            -0.3140, -1.0470, -0.5060, -0.3660,
-            -0.3140, -1.0470, -0.5060, -0.3660
+           -5.0000e+00, -5.0000e+00, -1.0000e+05,
+           -2.9671e+00, -1.8326e+00, -2.9671e+00, -3.1416e+00, -2.9671e+00, -8.7300e-02, -2.9671e+00,
+           -3.1400e-01, -1.0470e+00, -5.0600e-01, -3.6600e-01,
+           -3.4900e-01, -4.7000e-01, -1.2000e+00, -1.3400e+00,
+           -3.1400e-01, -1.0470e+00, -5.0600e-01, -3.6600e-01,
+           -3.1400e-01, -1.0470e+00, -5.0600e-01, -3.6600e-01,
+           -3.1400e+00,  0.0000e+00,  0.0000e+00, -1.5708e+00, -1.6700e+00, -1.5700e+00,
         ], device=self.device)
         self.robot_dof_upper_limits = torch.tensor([
-            2.8973,  1.7628,  2.8973, -0.0698,  2.8973,  3.7525,  2.8973,
-            2.2300, 1.0470, 1.8850, 2.0420,
-            2.0940, 2.4430, 1.9000, 1.8800,
-            2.2300, 1.0470, 1.8850, 2.0420,
-            2.2300, 1.0470, 1.8850, 2.0420
+            5.0000e+00, 5.0000e+00, 1.0000e+05,
+            2.9671e+00, 1.8326e+00, 2.9671e+00, 0.0000e+00, 2.9671e+00, 3.8223e+00, 2.9671e+00,
+            2.2300e+00, 1.0470e+00, 1.8850e+00, 2.0420e+00,
+            2.0940e+00, 2.4430e+00, 1.9000e+00, 1.8800e+00,
+            2.2300e+00, 1.0470e+00, 1.8850e+00, 2.0420e+00,
+            2.2300e+00, 1.0470e+00, 1.8850e+00, 2.0420e+00,
+            2.6180e+00, 3.1400e+00, 3.1400e+00, 1.5708e+00, 1.6700e+00, 1.5700e+00,
         ], device=self.device)
-        self.hand_dof_lower_limits = self.robot_dof_lower_limits[7:]
-        self.hand_dof_upper_limits = self.robot_dof_upper_limits[7:]
+
+        self.franka_dof_lower_limits = self.robot_dof_lower_limits[3:10]
+        self.franka_dof_upper_limits = self.robot_dof_upper_limits[3:10]
+        self.leap_dof_lower_limits = self.robot_dof_lower_limits[10:26]
+        self.leap_dof_upper_limits = self.robot_dof_upper_limits[10:26]
+        self.arx_dof_lower_limits = self.robot_dof_lower_limits[26:32]
+        self.arx_dof_upper_limits = self.robot_dof_upper_limits[26:32]
         self.abs_hand_actions = torch.zeros(1, 16, device=self.device)
         self.steps = 0
 
@@ -77,12 +95,10 @@ class WBCPolicyTransformer:
         full_pcd_eef_frame_t = torch.rand(N, 3, device=self.device) 
         # Generate random hand configuration (16,)
         q_hand = torch.rand(16, device=self.device)  # In [0, 1)
-        # Generate random EEF absolute pose (7,) - xyz + xyzw quaternion
-        eef_pos = torch.rand(3, device=self.device)  # xyz position in [0, 1)
-        eef_quat = torch.rand(4, device=self.device)  # xyzw quaternion in [0, 1)
-        eef_quat = eef_quat / torch.norm(eef_quat)  # Normalize to get valid quaternion
-        eef_abs_pose = torch.cat([eef_pos, eef_quat])
-        return full_pcd_eef_frame_t, q_hand, eef_abs_pose
+        q_arm_manip = torch.rand(7, device=self.device)  # In [0, 1)
+        q_arm_vision = torch.rand(6, device=self.device)  # In [0, 1)
+
+        return full_pcd_eef_frame_t, q_hand, q_arm_manip, q_arm_vision
 
     def load_checkpoint(self, checkpoint_path):
         checkpoint = torch.load(checkpoint_path, map_location=self.device)
@@ -97,7 +113,42 @@ class WBCPolicyTransformer:
         self.model.load_state_dict(new_state_dict)
         return checkpoint["success_rate_ep"]
 
-    def unnormalize_leap_hand_joints(self, joint_angles: torch.Tensor, delta: bool = False) -> torch.Tensor:
+    def normalize_robot_joints(self, joint_angles: torch.Tensor, robot: bool, delta: bool = False) -> torch.Tensor:
+        """
+        Normalize joint angles to be within [-1, 1].
+        Args:
+            joint_angles (torch.Tensor): (num_envs, num_robot_dofs)
+        Returns:
+            joint_angles (torch.Tensor): (num_envs, num_robot_dofs)
+        """
+        if robot=="franka":
+            assert joint_angles.shape[-1] == 7
+            lower_limits = self.franka_dof_lower_limits
+            upper_limits = self.franka_dof_upper_limits
+        elif robot=="leap":
+            assert joint_angles.shape[-1] == 16
+            lower_limits = self.leap_dof_lower_limits
+            upper_limits = self.leap_dof_upper_limits
+        elif robot=="arx":
+            assert joint_angles.shape[-1] == 6
+            lower_limits = self.arx_dof_lower_limits
+            upper_limits = self.arx_dof_upper_limits
+        else:
+            raise ValueError("robot must be in ['franka', 'leap', 'arx']")
+
+        franka_limit_range = upper_limits - lower_limits
+
+        if delta:
+            normalized = joint_angles / franka_limit_range
+        else:
+            desired_lower_limits = -1 * torch.ones_like(joint_angles)
+            desired_upper_limits = 1 * torch.ones_like(joint_angles)
+            normalized = (joint_angles - lower_limits) / franka_limit_range * (
+                desired_upper_limits - desired_lower_limits
+            ) + desired_lower_limits
+        return normalized
+
+    def unnormalize_robot_joints(self, joint_angles: torch.Tensor, robot: bool, delta: bool = False) -> torch.Tensor:
         """
         Unnormalize joint angles.
         Args:
@@ -105,16 +156,29 @@ class WBCPolicyTransformer:
         Returns:
             joint_angles (torch.Tensor): (num_envs, num_robot_dofs)
         """
-        assert joint_angles.shape[-1] == 16
-        lower_limits = self.hand_dof_lower_limits
-        upper_limits = self.hand_dof_upper_limits
-        limit_range = upper_limits - lower_limits
+        if robot=="franka":
+            assert joint_angles.shape[-1] == 7
+            lower_limits = self.franka_dof_lower_limits
+            upper_limits = self.franka_dof_upper_limits
+        elif robot=="leap":
+            assert joint_angles.shape[-1] == 16
+            lower_limits = self.leap_dof_lower_limits
+            upper_limits = self.leap_dof_upper_limits
+        elif robot=="arx":
+            assert joint_angles.shape[-1] == 6
+            lower_limits = self.arx_dof_lower_limits
+            upper_limits = self.arx_dof_upper_limits
+        else:
+            raise ValueError("robot must be in ['franka', 'leap', 'arx']")
+
+        franka_limit_range = upper_limits - lower_limits
+
         if delta:
-            unnormalized = joint_angles * limit_range
+            unnormalized = joint_angles * franka_limit_range
         else:
             desired_lower_limits = -1 * torch.ones_like(joint_angles)
             desired_upper_limits = 1 * torch.ones_like(joint_angles)
-            unnormalized = (joint_angles - desired_lower_limits) * limit_range / (
+            unnormalized = (joint_angles - desired_lower_limits) * franka_limit_range / (
                 desired_upper_limits - desired_lower_limits
             ) + lower_limits
         return unnormalized
@@ -125,14 +189,14 @@ class WBCPolicyTransformer:
 
         Args:
             obs_dict (OrderedDict):
-                full_pcd_eef_frame_t (torch.Tensor): (1, N, 3), N should be larger than num_local_points
+                full_pcd_frankabase_frame_t (torch.Tensor): (1, N, 3), N should be larger than num_local_points
                 q_hand (torch.Tensor): (1, 16)
         Returns:
             step_actions (torch.Tensor): (action_dim)
         """
-        obs_dict["local_pcd_t"], _ = crop_local_pcd(
-            obs_dict['full_pcd_eef_frame_t'], self.local_pcd_range, self.num_local_points
-        ) 
+        obs_dict["full_pcd_t"], _ = crop_local_pcd(
+            obs_dict['full_pcd_frankabase_frame_t'], self.local_pcd_range, self.num_local_points, is_cylindrical=True,
+        )
 
         with torch.no_grad():
             self.model.eval()
@@ -142,14 +206,13 @@ class WBCPolicyTransformer:
         # get the step action that goes into env.step() in sim
         step_actions = torch.clamp(student_actions, -self.clip_actions, self.clip_actions)
         return step_actions, obs_dict
-    
 
-    def get_action(self, full_pcd_eef_frame_t, q_hand, eef_abs_pose):
+    def get_action(self, full_pcd_frankabase_frame_t, q_hand, q_arm_manip, q_arm_vision):
         """
         get the final action for execution
 
         Args:
-            full_pcd_eef_frame_t (torch.Tensor): (N, 3), N should be larger than num_local_points
+            full_pcd_frankabase_frame_t (torch.Tensor): (N, 3), N should be larger than num_local_points
             q_hand (torch.Tensor): (16,)
             eef_abs_pose (torch.Tensor): (7,) xyz + xyzw
         Returns:
@@ -157,51 +220,52 @@ class WBCPolicyTransformer:
         """
 
         # reformatting inputs
-        assert full_pcd_eef_frame_t.dim() == 2 \
-            and full_pcd_eef_frame_t.size(0) >= self.num_local_points \
-            and full_pcd_eef_frame_t.size(1) == 3
+        assert full_pcd_frankabase_frame_t.dim() == 2 \
+            and full_pcd_frankabase_frame_t.size(0) >= self.num_local_points \
+            and full_pcd_frankabase_frame_t.size(1) == 3
         # (1, N, 3)
-        full_pcd_eef_frame_t_b = full_pcd_eef_frame_t.unsqueeze(0).to(self.device) 
-        q_hand_b = q_hand.unsqueeze(0).to(self.device) # (1, 16)
+        full_pcd_frankabase_frame_t_b = full_pcd_frankabase_frame_t.unsqueeze(0).to(self.device)
+        q_hand_b = self.normalize_robot_joints(q_hand.unsqueeze(0).to(self.device), robot="leap", delta=False) # (1, 16)
+        q_arm_manip_b = self.normalize_robot_joints(q_arm_manip.unsqueeze(0).to(self.device), robot="franka", delta=False) # (1, 7)
+        q_arm_vision_b = self.normalize_robot_joints(q_arm_vision.unsqueeze(0).to(self.device), robot="arx", delta=False) # (1, 7)
+
         obs_dict = OrderedDict([
-            ("full_pcd_eef_frame_t", full_pcd_eef_frame_t_b),
+            ("full_pcd_frankabase_frame_t", full_pcd_frankabase_frame_t_b),
+            ("q_arm_manip", q_arm_manip_b),
+            ("q_arm_vision", q_arm_vision_b),
             ("q_hand", q_hand_b),
             ("q_hand_ctrl_delta", (q_hand_b - self.abs_hand_actions)*2) # *2 helps with sim-to-real
         ])
         # inference policy
         step_action, obs_dict = self.inference_policy(obs_dict)
 
-        # apply policy action
-        pos_actions = step_action[:3] * self.action_scale["eef_pos"]
-        ctrl_target_eef_pos = eef_abs_pose[:3] + pos_actions
+        # get unnormalized absolute actions for execution
+        actions_abs = step_action.clone() # (32,)
 
-        # Interpret actions as target rot (axis-angle) displacements
-        rot_actions = step_action[3:6] * self.action_scale["eef_rot"]
-        angle = torch.norm(rot_actions, p=2, dim=-1)
-        axis = rot_actions / angle.unsqueeze(-1)
-        rot_actions_quat = quat_from_angle_axis(angle, axis)
+        actions_abs[:3] = step_action[:3] # base vel TODO: we should keep here as velocity right?
 
-        # clamp tiny rotations to avoid numerical issues
-        if angle < 1.0e-6:
-            rot_actions_quat = torch.tensor([0.0, 0.0, 0.0, 1.0], device=self.device)
+        if self.delta_franka_action:
+            actions_abs[3:10] = self.unnormalize_robot_joints(actions_abs[3:10], robot="franka", delta=True) * self.action_scale["franka"] + q_arm_manip
+        else:
+            actions_abs[3:10] = self.unnormalize_robot_joints(actions_abs[3:10], robot="franka", delta=False)
 
-        ctrl_target_eef_quat = quat_mul(
-            rot_actions_quat, eef_abs_pose[3:] # xyzw format
+        if self.delta_leap_action:
+            actions_abs[10:26] = self.unnormalize_robot_joints(actions_abs[10:26], robot="leap", delta=True) * self.action_scale["hand"] + q_hand
+        else:
+            actions_abs[10:26] = self.unnormalize_robot_joints(actions_abs[10:26], robot="leap", delta=False)
+
+        if self.delta_arx_action:
+            actions_abs[26:] = self.unnormalize_robot_joints(actions_abs[26:], robot="arx", delta=True) * self.action_scale["arx"] + q_arm_vision
+        else:
+            actions_abs[26:] = self.unnormalize_robot_joints(actions_abs[26:], robot="arx", delta=False)
+
+        actions_abs = tensor_clamp(
+            actions_abs, self.robot_dof_lower_limits, self.robot_dof_upper_limits
         )
+        self.abs_hand_actions[0, :] = actions_abs[10:26]
 
-        hand_actions = step_action[6:] * self.action_scale["hand"]
-        delta_hand_joint_actions_unnormalized = self.unnormalize_leap_hand_joints(hand_actions, delta=True)
+        return actions_abs.cpu().numpy(), obs_dict
 
-        abs_hand_actions = q_hand + delta_hand_joint_actions_unnormalized
-        abs_hand_actions = tensor_clamp(
-            abs_hand_actions, self.hand_dof_lower_limits, self.hand_dof_upper_limits
-        )
-
-        self.abs_hand_actions[0, :] = abs_hand_actions
-        self.steps += 1
-
-        return ctrl_target_eef_pos.cpu().numpy(), ctrl_target_eef_quat.cpu().numpy(), abs_hand_actions.cpu().numpy(), obs_dict
-    
 
 
 if __name__ == "__main__":
@@ -219,7 +283,7 @@ if __name__ == "__main__":
     print(f"profiling with {test_num} inferences")
     t_3 = time.time()
     for i in range(int(test_num)):
-        test_input = generate_random_inputs()
+        test_input = model.generate_random_inputs()
         action = model.get_action(*test_input)
     t_4 = time.time()
     t_test = t_4 - t_3
