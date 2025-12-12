minor normalization fix

isaacgymenvs/distillation/dagger_mobile_trainer.py
@@ -183,7 +183,7 @@ class DaggerMobile:
                 }
             )
 
-    # TODO: teacher loading utils, shall I just simply merge them?
+    # teacher loading utils
     def load_param_dict(self, cfg_path) -> Dict:
         base_dir = os.path.dirname(__file__)
         full_path = os.path.join(base_dir, cfg_path)
@@ -205,7 +205,7 @@ class DaggerMobile:
         if self.normalize_input and 'running_mean_std' in weights:
             model.running_mean_std.load_state_dict(weights["running_mean_std"])
 
-    def save_checkpoint(self, episode, success_rate_ep=None, top_k=3): # TODO
+    def save_checkpoint(self, episode, success_rate_ep=None, top_k=3):
         checkpoint = {
             "episode": episode,
             "model_state_dict": self.student_model.state_dict(),
@@ -338,7 +338,6 @@ class DaggerMobile:
             else:
                 obs_student["full_pcd_t"] = downsample_pcd_batched(obs["full_pcd_t"], num_points_full_pcd_t)
 
-        # TODO: maybe update this to a cylindrical local crop
         if "local_pcd_t" in self.pcd_encoders_keys:
             obs_student["local_pcd_t"], crop_logs = crop_local_pcd(obs['full_pcd_t'], self.local_pcd_range, self.num_local_points, is_cylindrical=True) # (num_envs, num_local_points, 3)
             if self.use_wandb:
@@ -402,7 +401,7 @@ class DaggerMobile:
             obs_input_a0["q_arm_vision"] = self.env.normalize_robot_joints(q_arm_vision, robot="arx", delta=False)
             obs_input_a0["q_hand"] = self.env.normalize_robot_joints(q_hand, robot="leap", delta=False)
             if "q_hand_ctrl_delta" in self.state_encoders_keys:
-                obs_input_a0["q_hand_ctrl_delta"] = q_hand - self.env.abs_actions[:, 10:26] # TODO: small bug here? this should be normalized
+                obs_input_a0["q_hand_ctrl_delta"] = self.env.normalize_robot_joints(q_hand - self.env.abs_actions[:, 10:26], robot="leap", delta=True)
 
             with torch.no_grad():
                 student_model = self.student_model.module if self.multi_gpu else self.student_model
@@ -477,8 +476,8 @@ class DaggerMobile:
             if self.scheduler is not None:
                 self.scheduler.step()
 
-            mem_allocated_GB = torch.cuda.memory_allocated() / 1024**3
-            mem_reserved_GB = torch.cuda.memory_reserved() / 1024**3
+            mem_allocated_GB = float(torch.cuda.memory_allocated() / 1024**3)
+            mem_reserved_GB = float(torch.cuda.memory_reserved() / 1024**3)
 
             if self.use_wandb:
                 wandb.log({

isaacgymenvs/inference/inference_wbc_policy.py
@@ -82,7 +82,7 @@ class WBCPolicyTransformer:
         self.leap_dof_upper_limits = self.robot_dof_upper_limits[10:26]
         self.arx_dof_lower_limits = self.robot_dof_lower_limits[26:32]
         self.arx_dof_upper_limits = self.robot_dof_upper_limits[26:32]
-        self.abs_hand_actions = torch.zeros(1, 16, device=self.device)
+        self.abs_hand_actions = torch.zeros(16, device=self.device)
         self.steps = 0
 
     def generate_random_inputs(self):
@@ -226,6 +226,9 @@ class WBCPolicyTransformer:
         # (1, N, 3)
         full_pcd_frankabase_frame_t_b = full_pcd_frankabase_frame_t.unsqueeze(0).to(self.device)
         q_hand_b = self.normalize_robot_joints(q_hand.unsqueeze(0).to(self.device), robot="leap", delta=False) # (1, 16)
+        q_hand_ctrl_delta_b = self.normalize_robot_joints(
+            (q_hand.to(self.device) - self.abs_hand_actions), robot="leap", delta=True
+        )
         q_arm_manip_b = self.normalize_robot_joints(q_arm_manip.unsqueeze(0).to(self.device), robot="franka", delta=False) # (1, 7)
         q_arm_vision_b = self.normalize_robot_joints(q_arm_vision.unsqueeze(0).to(self.device), robot="arx", delta=False) # (1, 7)
 
@@ -234,7 +237,7 @@ class WBCPolicyTransformer:
             ("q_arm_manip", q_arm_manip_b),
             ("q_arm_vision", q_arm_vision_b),
             ("q_hand", q_hand_b),
-            ("q_hand_ctrl_delta", (q_hand.unsqueeze(0).to(self.device) - self.abs_hand_actions)*2) # *2 helps with sim-to-real
+            ("q_hand_ctrl_delta", q_hand_ctrl_delta_b*2) # *2 helps with sim-to-real
         ])
         # inference policy
         step_action, obs_dict = self.inference_policy(obs_dict)
@@ -262,7 +265,7 @@ class WBCPolicyTransformer:
         actions_abs = tensor_clamp(
             actions_abs, self.robot_dof_lower_limits, self.robot_dof_upper_limits
         )
-        self.abs_hand_actions[0, :] = actions_abs[10:26]
+        self.abs_hand_actions[:] = actions_abs[10:26]
 
         return actions_abs.cpu().numpy(), obs_dict
 