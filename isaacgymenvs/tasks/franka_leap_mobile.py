aa9f3c16623b63126b8b29315a624433e6df3b36
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-01-08 16:23:18 -0500

    adjust table height rand, tune cam init config

--- isaacgymenvs/tasks/franka_leap_mobile.py
+++ isaacgymenvs/tasks/franka_leap_mobile.py
@@ -184,7 +184,7 @@ class FrankaLEAPMobile(VecTask):
                      0.0, 0.0, 0.0, 0.0,
                      0.0, 0.0, 0.0, 0.0,
                      0.0, 0.0, 0.0, 0.0,] + \
-                    [0.0, 0.4, 0.4, 0.0, 0.0, 0.0]
+                    [0.0, 0.6, 1.4, -0.8, 0.0, 0.0]
                 ] * self.num_envs
             ).to(self.device)
             self.canonical_joint_config[:, :3] = base_init_pose
