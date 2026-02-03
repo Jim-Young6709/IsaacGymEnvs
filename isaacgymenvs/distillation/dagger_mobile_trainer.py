43f2e1a3c8459c28752adaeaba978e7808dee174
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-02-02 22:47:49 -0500

    update distillation

--- isaacgymenvs/distillation/dagger_mobile_trainer.py
+++ isaacgymenvs/distillation/dagger_mobile_trainer.py
@@ -188,6 +188,10 @@ class DaggerMobile:
                 }
             )
 
+        # aux
+        self.aux_switch_steps = 30000
+        self.aux_buffer = torch.zeros(self.env.num_envs, 1, 3, device=self.device)
+
     # teacher loading utils
     def load_param_dict(self, cfg_path) -> Dict:
         base_dir = os.path.dirname(__file__)
@@ -452,18 +456,21 @@ class DaggerMobile:
                 point_base_frame = torch.bmm(point_shifted, rot_global2base) # (num_envs, N, 3), bmm is like matmul but specifically made for batches of 2D matrices (input has to be 3D), faster than matmul
                 obs_input_a0["objxyz_t0"] = point_base_frame[:, 0, :] # (num_envs, 3)
             if "aux_object_state" in self.state_encoders_keys:
-                object_center_pos = self.env.states["object_center_pos"].clone()
+                if self.total_steps < self.aux_switch_steps:
+                    object_center_pos = self.env.states["object_center_pos"].clone()
 
-                franka_base_pos = self.env.states['franka_base_pose7'][:, :3] # (num_envs, 3)
-                franka_base_quat = self.env.states['franka_base_pose7'][:, 3:] # (num_envs, 4)
-                franka_base_rot_mat = quaternion_to_matrix_ig(franka_base_quat)
-                rot_global2base = franka_base_rot_mat.transpose(1, 2) # (num_envs, 3, 3)
+                    franka_base_pos = self.env.states['franka_base_pose7'][:, :3] # (num_envs, 3)
+                    franka_base_quat = self.env.states['franka_base_pose7'][:, 3:] # (num_envs, 4)
+                    franka_base_rot_mat = quaternion_to_matrix_ig(franka_base_quat)
+                    rot_global2base = franka_base_rot_mat.transpose(1, 2) # (num_envs, 3, 3)
 
-                point_shifted = (object_center_pos - franka_base_pos).unsqueeze(1) # (num_envs, 1, 3)
-                point_base_frame = torch.bmm(point_shifted, rot_global2base) # (num_envs, N, 3), bmm is like matmul but specifically made for batches of 2D matrices (input has to be 3D), faster than matmul
-                object_center_pos = point_base_frame[:, 0, :] # (num_envs, 3) object center pos in franka base frame
+                    point_shifted = (object_center_pos - franka_base_pos).unsqueeze(1) # (num_envs, 1, 3)
+                    point_base_frame = torch.bmm(point_shifted, rot_global2base) # (num_envs, N, 3), bmm is like matmul but specifically made for batches of 2D matrices (input has to be 3D), faster than matmul
+                    object_center_pos = point_base_frame[:, 0, :] # (num_envs, 3) object center pos in franka base frame
 
-                obs_input_a0["aux_object_state"] = object_center_pos + 0.1 * torch.rand(self.env.num_envs, 3, device=self.device) # purely white noise for now TODO: update this later
+                    obs_input_a0["aux_object_state"] = object_center_pos + 0.05 * torch.rand(self.env.num_envs, 3, device=self.device) # purely white noise for now TODO: update this later
+                else:
+                    obs_input_a0["aux_object_state"] = self.aux_buffer.clone()
 
             # Viser debug utils
             # env_id = self.env.viser_visualizer.env_id
@@ -477,6 +484,8 @@ class DaggerMobile:
                 student_model.eval()
                 output = student_model(obs_input_a0)
                 student_actions_chunk = output["action"]
+                if self.student_model.aux_prediction:
+                    self.aux_buffer[:] = output["aux"].clone()
 
             teacher_preds_buffer = []
 
@@ -648,18 +657,7 @@ class DaggerMobile:
                 point_base_frame = torch.bmm(point_shifted, rot_global2base) # (num_envs, N, 3), bmm is like matmul but specifically made for batches of 2D matrices (input has to be 3D), faster than matmul
                 obs_input_a0["objxyz_t0"] = point_base_frame[:, 0, :] # (num_envs, 3)
             if "aux_object_state" in self.state_encoders_keys:
-                object_center_pos = self.env.states["object_center_pos"].clone()
-
-                franka_base_pos = self.env.states['franka_base_pose7'][:, :3] # (num_envs, 3)
-                franka_base_quat = self.env.states['franka_base_pose7'][:, 3:] # (num_envs, 4)
-                franka_base_rot_mat = quaternion_to_matrix_ig(franka_base_quat)
-                rot_global2base = franka_base_rot_mat.transpose(1, 2) # (num_envs, 3, 3)
-
-                point_shifted = (object_center_pos - franka_base_pos).unsqueeze(1) # (num_envs, 1, 3)
-                point_base_frame = torch.bmm(point_shifted, rot_global2base) # (num_envs, N, 3), bmm is like matmul but specifically made for batches of 2D matrices (input has to be 3D), faster than matmul
-                object_center_pos = point_base_frame[:, 0, :] # (num_envs, 3) object center pos in franka base frame
-
-                obs_input_a0["aux_object_state"] = object_center_pos + 0.1 * torch.rand(self.env.num_envs, 3, device=self.device) # purely white noise for now TODO: update this later
+                obs_input_a0["aux_object_state"] = self.aux_buffer.clone()
 
             # Viser debug utils
             # env_id = self.env.viser_visualizer.env_id
@@ -673,6 +671,8 @@ class DaggerMobile:
                 student_model.eval()
                 output = student_model(obs_input_a0)
                 student_actions_chunk = output["action"]
+                if self.student_model.aux_prediction:
+                    self.aux_buffer[:] = output["aux"].clone()
 
             for action_idx in range(self.chunk_size):
                 # # get teacher action
