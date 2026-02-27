0389a66b83a6aaed573cd09e26f72cc1e826b931
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-02-27 05:08:02 -0500

    bugfix

--- isaacgymenvs/distillation/dagger_mobile_trainer.py
+++ isaacgymenvs/distillation/dagger_mobile_trainer.py
@@ -41,6 +41,7 @@ class DaggerMobile:
             self.global_rank = int(os.getenv("RANK", "0"))
             self.world_size = int(os.getenv("WORLD_SIZE", "1"))   
 
+            cfg.task.env.scene.batch_idx = self.global_rank
             cfg.sim_device = f"cuda:{self.local_rank}"
             cfg.rl_device = f"cuda:{self.local_rank}"
             torch.cuda.set_device(self.local_rank)
