05a1717567c9e4217ba4c15c3df5c6077e7643cc
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-02-28 00:51:23 -0500

    *

--- scripts/pre_sample_scene_states_table_multi.sh
+++ scripts/pre_sample_scene_states_table_multi.sh
@@ -3,17 +3,27 @@ set -euo pipefail
 
 # Set these two variables only.
 TABLE_HEIGHT_RANGES=(
-  "0.7 0.8"
-  "0.8 0.8"
+  "0.0 0.0"
   "0.0 0.0"
   "0.0 0.1"
+  "0.7 0.8"
+  "0.8 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
+  "0.0 0.8"
   "0.0 0.8"
 )
 NUM_ENVS_LIST=(1024)
 
-BASE_INIT_RANGE="[[-1.0, -0.25, -0.3], [-0.2, 0.25, 0.3]]"
+BASE_INIT_RANGE="[[-0.3, -0.5, -0.3], [-0.2, 0.5, 0.3]]"
 SEED=1
-TASK_NAME="table_multi"
+TASK_NAME="table_multi_close"
 TEACHER_CKPT="./ckpts/exp_table_Feb23.pth"
 SCENE_DIR="./presampled_envs/scene_only"
 HEADLESS="True"
@@ -31,7 +41,7 @@ for num_envs in "${NUM_ENVS_LIST[@]}"; do
     fi
 
     sample_idx=$((range_idx + 1))
-    hdf5_name="${TASK_NAME}_${height_tag}tableheight_${num_envs}_idx${sample_idx}.hdf5"
+    hdf5_name="${TASK_NAME}_idx${sample_idx}_${height_tag}tableheight_${num_envs}.hdf5"
     scene_hdf5_path="${SCENE_DIR}/${hdf5_name}"
 
     python isaacgymenvs/presampling/pre_sample_scene_states_table_multi.py \
