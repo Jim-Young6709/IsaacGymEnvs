104a44a3f77eaa9b01a4bb615de902a106772888
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-04-16 20:04:04 -0400

    update shelf grasp

--- isaacgymenvs/tasks/franka_leap_pick_side.py
+++ isaacgymenvs/tasks/franka_leap_pick_side.py
@@ -32,6 +32,8 @@ class FrankaLEAPPickSide(FrankaLEAP):
             force_render=force_render
         )
 
+    def _post_init_buffers(self):
+        super()._post_init_buffers()
         if self.eef_init["enable"]:
             dis_open_range = self.eef_init["dis_open_range"]
             dis_open = torch.rand(self.num_envs, device=self.device) * (dis_open_range[1] - dis_open_range[0]) + dis_open_range[0]
@@ -55,6 +57,9 @@ class FrankaLEAPPickSide(FrankaLEAP):
 
             # TODO: resampling mechanism here when IK failed
             self.canonical_joint_config[:, :7] = self.get_joint_from_ee(eef_init_pos7)
+
+            self.default_reset_joint_config = self.canonical_joint_config.clone()
+            self.ik_regularization_config = self.canonical_joint_config[:, :7]
 
     def _create_envs(self, spacing, num_per_row):
         """
@@ -96,6 +101,7 @@ class FrankaLEAPPickSide(FrankaLEAP):
 
         # load all meshes first
         all_meshes_list = self.create_all_meshes()
+        self.num_objects = len(all_meshes_list) # @ray record number of objects for per-object success rate tracking
 
         # Create environments
         for i in tqdm(range(self.num_envs), desc="Creating Envs"):
@@ -273,11 +279,17 @@ class FrankaLEAPPickSide(FrankaLEAP):
     def _update_states(self):
         super()._update_states()
         eef_rot_mat = quaternion_to_matrix_ig(self._eef_state[:, 3:7])
-        box_to_eef_rot_6d = matrix_to_rotation_6d(eef_rot_mat.transpose(1, 2)) # since its eef policy, so box quat can always be [0, 0, 0, 1]
+        eef_rot_mat_T = eef_rot_mat.transpose(1, 2)
+        box_rot_mat = quaternion_to_matrix_ig(self.box_quats)
+        box_to_eef_rot_mat = torch.matmul(eef_rot_mat_T, box_rot_mat)
+        box_to_eef_rot_6d = matrix_to_rotation_6d(box_to_eef_rot_mat)
+
+        box_to_eef_world = self.box_pos - self._eef_state[:, :3] # note box_pos here is box bottom center not box center
+        box_to_eef = torch.matmul(eef_rot_mat_T, box_to_eef_world.unsqueeze(-1)).squeeze(-1)
 
         self.states.update({
             # Box region
-            "box_to_eef_pos": self.box_pos - self._eef_state[:, :3],
+            "box_bottom_to_eef": box_to_eef,
             "box_dims": self.box_dims,
             "box_to_eef_rot_6d": box_to_eef_rot_6d,
             "obj_to_box_center_xy": self._object_state[:, :2] - self.box_pos[:, :2],
@@ -285,12 +297,6 @@ class FrankaLEAPPickSide(FrankaLEAP):
             "lift": ~self.box_bottom_collision,
             "collision": self.box_wall_collision & (not self.scene_box_cfg["colli_reset"]),
         })
-
-    def _reset_box_state(self):
-        z_shift = self.scene_box_cfg["z"]
-        r_rot_ex = self.scene_box_cfg["r"]
-        rot_in = self.scene_box_cfg["rot_in"]
-        rot_ex = self.scene_box_cfg["rot_ex"]
 
     def check_robot_collision(self):
         super().check_robot_collision()
@@ -303,7 +309,7 @@ class FrankaLEAPPickSide(FrankaLEAP):
         obs_components = ["q_hand",
                           "eef_finger1_pos_relative", "eef_finger2_pos_relative",
                           "eef_finger3_pos_relative", "eef_finger4_pos_relative",
-                          "box_to_eef_pos", "box_dims", "box_to_eef_rot_6d",
+                          "box_bottom_to_eef", "box_dims", "box_to_eef_rot_6d",
                           "object_to_eef", "object_to_eef_rot_6d",
                           "target_to_eef", "target_to_eef_rot_6d"]
 
@@ -311,7 +317,7 @@ class FrankaLEAPPickSide(FrankaLEAP):
                              "eef_pos", "eef_rot_6d", "eef_vel",
                              "eef_finger1_pos_relative", "eef_finger2_pos_relative",
                              "eef_finger3_pos_relative", "eef_finger4_pos_relative",
-                             "box_to_eef_pos", "box_dims", "box_to_eef_rot_6d",
+                             "box_bottom_to_eef", "box_dims", "box_to_eef_rot_6d",
                              "object_to_eef", "object_to_eef_rot_6d",
                              "target_to_eef", "target_to_eef_rot_6d"]
 
@@ -399,7 +405,7 @@ def compute_franka_leap_reward(states, reward_settings):
         r_lift = torch.where(states["lift"], 1.0, torch.zeros_like(object_height))
 
     # R3: Object goal distance reward (based on average point matching distance)
-    d_eef_point_goal = states["point_matching_err"]
+    d_eef_point_goal = states["point_matching_err_target"]
     beta_object_goal = reward_settings["beta_object_goal"]
     r_obj_goal = torch.exp(-beta_object_goal * d_eef_point_goal)
     r_obj_goal = torch.where(states["lift"], r_obj_goal, 0.0)
@@ -464,7 +470,7 @@ def launch_test(cfg: DictConfig):
     for i in tqdm(range(1000)):
         t1 = time.time()
         env.reset_idx()
-        # env.set_robot_joint_state(env.canonical_joint_config)
+        env.set_robot_joint_state(env.canonical_joint_config)
         # env.set_robot_joint_state(env.canonical_grasp_config)
         env.step_sim_multi(1, False)
         env.compute_observations()
