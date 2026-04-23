b27b11a4abe392cffd377e356e4824b30832d4be
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-04-22 21:35:11 -0400

    minor fix

--- isaacgymenvs/distillation/dagger_mobile_multi_trainer.py
+++ isaacgymenvs/distillation/dagger_mobile_multi_trainer.py
@@ -59,8 +59,7 @@ class DaggerMobileMultiExp:
             expert_idx = self.local_rank % num_experts
             cfg.teacher.ckpt = cfg.teacher.ckpt[expert_idx]
             cfg.task.env.mesh.mesh_dir = cfg.task.env.mesh.mesh_dir[expert_idx]
-            cfg.task.env.numObservations = cfg.task.env.numObservations[expert_idx]
-            cfg.task.env.numStates = cfg.task.env.numStates[expert_idx]
+            cfg.task.cfg_overrideb = cfg.task.cfg_overrideb[expert_idx]
             cfg.task.env.scene.hdf5_path = cfg.task.env.scene.hdf5_path[expert_idx]
 
         self.cfg = cfg
