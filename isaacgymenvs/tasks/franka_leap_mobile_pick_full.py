bc4137b1b7e323f79120200e6876fcf20746e773
Jim-Young6709 <jiahuiyang6709@outlook.com>
2026-05-16 03:16:49 -0400

    minor fix, shelf rl target can't be tuned too much

--- isaacgymenvs/tasks/franka_leap_mobile_pick_full.py
+++ isaacgymenvs/tasks/franka_leap_mobile_pick_full.py
@@ -453,7 +453,7 @@ class FrankaLEAPMobilePickFull(FrankaLEAPMobile):
             pass
         elif self.cfg_override == "SideConstrained":
             self.obj_pos_target[:] = self.box_pos.clone()
-            self.obj_pos_target[:, 0] -= (self.box_dims[:, 0] / 2 + 0.2)
+            self.obj_pos_target[:, 0] -= (self.box_dims[:, 0] / 2 + 0.1)
             self.obj_pos_target[:, 2] += 0.1 # self.box_dims[:, 2] / 2
 
         self.switching_target_pos = self.states['object_center_pos'].clone()
