504fe7001356616c34c4379c7cf07fecd736061d
DaedalusDelta <muxinl@andrew.cmu.edu>
2026-05-22 04:49:05 -0400

    Support side manifest overrides in multi-teacher

--- isaacgymenvs/distillation/dagger_mobile_multi_trainer.py
+++ isaacgymenvs/distillation/dagger_mobile_multi_trainer.py
@@ -513,6 +513,7 @@ class DaggerMobileMultiExp:
             "task.env.enable_viser",
             "task.env.teacher_obs_action_frame",
             "task.env.mesh.mesh_dir",
+            "task.env.mesh.variant_manifest_json",
             "task.env.grasp_guide_idx",
             "task.env.object_settings.mass_range",
             "task.env.object_teleport.enable",
@@ -528,6 +529,7 @@ class DaggerMobileMultiExp:
             "task.env.verified_teacher_bank.sampling_probs.afar",
             "task.env.verified_teacher_bank.sampling_probs.near_recovery",
             "task.env.verified_teacher_bank.sampling_probs.far_recovery",
+            "task.env.verified_teacher_bank.sampling_probs.failure_recovery",
         )
         return {path: self._get_cfg_path(cfg, path) for path in paths if self._has_cfg_path(cfg, path)}
 
